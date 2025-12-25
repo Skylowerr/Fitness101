17f96da84ee16e7c5bc9f3663b7630f10f26c9da
Skylowerr <gokceemirhan23@gmail.com>
2025-12-25 02:15:32 +0100

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Emirhan Gökçe on 25.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
